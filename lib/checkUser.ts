91f737aa48baede9bad79c8e7d7f69a1d5f52984
moses_mac <moseslee1999@gmail.com>
2025-09-27 00:28:42 +0800

    Add clerk

---
+++ lib/checkUser.ts
@@ -0,0 +1,37 @@
+//Get user from clerk
+//Find the user in the db
+// If user contain in db return user
+// else create user in db and return user
+
+import { currentUser } from "@clerk/nextjs/server";
+
+import { db } from "./db";
+
+export const checkUser = async () => {
+  const user = await currentUser();
+
+  if (!user) {
+    return null;
+  }
+
+  const loggedInUser = await db.user.findUnique({
+    where: {
+      clerkUserId: user.id,
+    },
+  });
+
+  if (loggedInUser) {
+    return loggedInUser;
+  }
+
+  const newUser = await db.user.create({
+    data: {
+      clerkUserId: user.id,
+      name: `${user.firstName} ${user.lastName}`,
+      imageUrl: user.imageUrl,
+      email: user.emailAddresses[0]?.emailAddress,
+    },
+  });
+
+  return newUser;
+};
