7878f7175b8ce82da49598d9a83b27325956a222
moses_mac <moseslee1999@gmail.com>
2025-09-28 00:43:44 +0800

    Feat: Add new pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { currentUser } from "@clerk/nextjs/server";
+import Guest from "@/components/Guest";
 
-export default function HomePage() {
-  return <div className="text-red-500">HomePage</div>;
+export default async function HomePage() {
+  const user = await currentUser();
+  if (!user) {
+    return <Guest />;
+  } else {
+    return <div className="text-red-500">HomePage</div>;
+  }
 }
